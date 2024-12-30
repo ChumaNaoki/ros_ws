d481b5b36ff1d8ccdd824c9f602be623b2142a7b
Chuma Naoki <s23C1091UY@s.chibakoudai.jp>
2024-12-30 23:34:39 +0900

    add

--- mypkg/mypkg/talker.py
+++ mypkg/mypkg/talker.py
@@ -1,25 +1,23 @@
-import rclpy
-from rclpy.node import Node
-from std_msgs.msg import Int16
-
-###ヘッダの下にTalkerというクラスを作成###
+import rclpy                     #ROS 2のクライアントのためのライブラリ
+from rclpy.node import Node      #ノードを実装するためのNodeクラス（クラスは第10回で）
+from std_msgs.msg import Int16   #通信の型（16ビットの符号付き整数）
 
 
+class Talker(Node):
+    def __init__(self):
+        super().__init__("talker")
+        self.pub = self.create_publisher(Int16, "countup", 10)
+        self.n = 0
+        self.create_timer(0.5, self.cb)
 
-rclpy.init()
-node = Node("talker")
-pub = node.create_publisher(Int16, "countup", 10)
-n = 0
-
-
-def cd():
-    global n
-    msg = Int16()
-    msg.data = n
-    pub.publish(msg)
-    n += 1
+    def cb(self):          #20行目で定期実行されるコールバック関数
+        msg = Int16()  #メッセージの「オブジェクト」
+        msg.data = self.n   #msgオブジェクトの持つdataにnを結び付け
+        self.pub.publish(msg)        #pubの持つpublishでメッセージ送信
+        self.n += 1
 
 
 def main():
-    node.create_timer(0.5, cd)
-    rclpy.spin(node)
+    rclpy.init()
+    node = Talker()
+    rclpy.spin(node)            #実行（無限ループ）
